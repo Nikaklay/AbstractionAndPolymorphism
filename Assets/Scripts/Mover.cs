c57740a29117893897e75785eb40a480633897ba
Nikaklay <n.a.ivanov33@gmail.com>
2026-06-23 17:42:11 +0300

    Update Mover script

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,20 +3,37 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Mover : MonoBehaviour
 {
-    [SerializeField] int _speed;
+    [SerializeField] float _speedMovement;
+    [SerializeField] float _speedRotation;
     private CharacterController _characterController;
+
+    private Quaternion _currentRotation;
 
 
 
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
+        _currentRotation = Quaternion.identity;
     }
 
     private void Update()
     {
         Vector3 direction = GetMovementDirection();
-        _characterController.Move(direction * _speed * Time.deltaTime);
+
+        _characterController.Move(direction * _speedMovement * Time.deltaTime);
+
+        Rotate(direction);
+    }
+
+    private void Rotate(Vector3 direction)
+    {
+        if (direction == Vector3.zero)
+            transform.rotation = _currentRotation;
+
+        Quaternion lookDirection = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookDirection, _speedRotation * Time.deltaTime);
+        _currentRotation = transform.rotation;
     }
 
     private Vector3 GetMovementDirection()
